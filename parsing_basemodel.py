fbb39741c7422174c1473c7fb3aa35e79ed77524
Adam Ghouaibia <adghouai@z2r5p2.42lyon.fr>
2026-04-28 16:19:16 +0200

    modified parsing

--- parsing_basemodel.py
+++ parsing_basemodel.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, field_validator, model_validator
-import random
 
 
 class ParsingError(Exception):
@@ -14,7 +13,7 @@ class ValidConfig(BaseModel):
     EXIT: tuple[int, int]
     OUTPUT_FILE: str
     PERFECT: bool
-    SEED: float
+    SEED: str | None
     CONFIG_FILE: str
 
     @field_validator("WIDTH", "HEIGHT", mode="before")
@@ -26,22 +25,22 @@ class ValidConfig(BaseModel):
             raise ParsingError(
                 f"Value {size} is not an int. Values can only be integers "
                 "and > 0 for WIDTH and HEIGHT"
-                )
+            )
         if parsed_size < 1:
             raise ParsingError(
-                f"Value {size} cannot be < 1 for WIDTH and HEIGHT"
-                )
+                f"Value {parsed_size} cannot be < 1 for WIDTH and HEIGHT"
+            )
         return parsed_size
 
     @field_validator("ENTRY", "EXIT", mode="before")
     @classmethod
-    def format_coordinates(cls, coordinates: str) -> tuple[str, str]:
+    def validate_coordinates(cls, coordinates: str) -> tuple[str, str]:
         split_coordinates = coordinates.split(",")
         if len(split_coordinates) != 2:
             raise ParsingError(
                 f"Wrong format for value: {coordinates}. Try "
                 "'value(int),value(int)' for ENTRY and EXIT"
-                )
+            )
         try:
             for value in split_coordinates:
                 _ = int(value)
@@ -49,7 +48,7 @@ class ValidConfig(BaseModel):
             raise ParsingError(
                 f"Value: {value} in {split_coordinates} is not an int. "
                 "Values can only be positive integers for ENTRY and EXIT"
-                )
+            )
         return (split_coordinates[0], split_coordinates[1])
 
     @field_validator("PERFECT", mode="before")
@@ -60,45 +59,49 @@ class ValidConfig(BaseModel):
         elif maze_type == "False":
             return False
         raise ParsingError(
-            f"Value '{type}' invalid. Maze type must be True, "
+            f"Value '{maze_type}' invalid. Maze type must be True, "
             "False or left empty for key 'PERFECT'"
-            )
+        )
 
     @field_validator("SEED", mode="before")
     @classmethod
-    def validate_seed(cls, seed: str) -> float:
+    def validate_seed(cls, seed: str) -> str | None:
         if seed == "":
-            return float(random.randrange(999999))
-        try:
-            parsed_seed = float(seed)
-        except Exception:
-            raise ParsingError("SEED must be and int, a float or left empty")
-        if parsed_seed < 0:
-            parsed_seed = parsed_seed * -1
-        return parsed_seed
+            return None
+        return seed
 
     @model_validator(mode="after")
-    def validate_file_name(self) -> BaseModel:
+    def validate_file_name(self) -> 'ValidConfig':
         if not self.OUTPUT_FILE.endswith('.txt'):
             raise ParsingError("Output file must end with '.txt'")
         elif self.OUTPUT_FILE == self.CONFIG_FILE:
             raise ParsingError(
                 "Output file cannot have the same name as config file"
-                )
+            )
         if self.ENTRY == self.EXIT:
             raise ParsingError(
                 "'ENTRY' and 'EXIT' cannot have the same coordinates"
-                )
+            )
         x, y = self.ENTRY
-        if x + 1 > self.WIDTH:
-            raise ParsingError("Coordinate 'x' of key 'ENTRY' is out of range")
-        elif y + 1 > self.HEIGHT:
-            raise ParsingError("Coordinate 'y' of key 'ENTRY' is out of range")
+        if x + 1 > self.WIDTH or x < 0:
+            raise ParsingError(
+                "Coordinate 'x' of key 'ENTRY' is out of range. "
+                "x must be < WIDTH and >= 0"
+            )
+        elif y + 1 > self.HEIGHT or y < 0:
+            raise ParsingError(
+                "Coordinate 'y' of key 'ENTRY' is out of range. "
+                "y must be < HEIGHT and >= 0")
         x, y = self.EXIT
-        if x + 1 > self.WIDTH:
-            raise ParsingError("Coordinate 'x' of key 'EXIT' is out of range")
-        elif y + 1 > self.HEIGHT:
-            raise ParsingError("Coordinate 'y' of key 'EXIT' is out of range")
+        if x + 1 > self.WIDTH or x < 0:
+            raise ParsingError(
+                "Coordinate 'x' of key 'EXIT' is out of range. "
+                "x must be < WIDTH and >= 0"
+            )
+        elif y + 1 > self.HEIGHT or y < 0:
+            raise ParsingError(
+                "Coordinate 'y' of key 'EXIT' is out of range. "
+                "y must be < HEIGHT and >= 0")
         return self
 
 
@@ -112,7 +115,7 @@ def fetch_config(file_name: str) -> ValidConfig:
             "OUTPUT_FILE",
             "PERFECT",
             "SEED"
-            ]
+        ]
         config: dict = {}
         with open(file_name, "r") as file:
             content = file.readline()
@@ -124,15 +127,18 @@ def fetch_config(file_name: str) -> ValidConfig:
                     if len(split_content) != 2:
                         raise ParsingError(
                             f"Wrong format for '{content}'. try 'KEY=value'"
-                            )
+                        )
                     key, value = split_content
                     if key not in keys:
                         raise ParsingError(
-                            f"Key '{key}' not valid. "
+                            f"Key: '{key}' not valid.\n"
                             f"List of valid keys: {keys}"
-                            )
+                        )
                     elif key in keys and key in config.keys():
-                        raise ParsingError(f"Key '{key}' found twice")
+                        raise ParsingError(
+                            f"Key: '{key}' found twice, "
+                            "cannot have duplicate keys"
+                        )
                     config[key] = value.rstrip()
                 content = file.readline()
             if len(keys) != len(config.keys()):
@@ -141,7 +147,7 @@ def fetch_config(file_name: str) -> ValidConfig:
                     if key not in config.keys():
                         missing.append(key)
                 raise ParsingError(f"Keys: {missing} missing from config file")
-            config = ValidConfig(
+            valid_model = ValidConfig(
                 WIDTH=config['WIDTH'],
                 HEIGHT=config['HEIGHT'],
                 ENTRY=config['ENTRY'],
@@ -150,16 +156,14 @@ def fetch_config(file_name: str) -> ValidConfig:
                 PERFECT=config['PERFECT'],
                 SEED=config['SEED'],
                 CONFIG_FILE=file_name
-                )
-        return config
-    except Exception as e:
-        raise (e)
+            )
+        return valid_model
+    except FileNotFoundError:
+        raise ParsingError(f"File: '{file_name}' not found")
+    except Exception:
+        raise ParsingError("Unexpected error occured during parsing")
 
 
 if __name__ == "__main__":
-    try:
-        config = fetch_config("config.txt")
-        config = config.model_dump()
-        print(config)
-    except Exception as e:
-        print(e)
+    config = fetch_config("config.txt")
+    print(config)
