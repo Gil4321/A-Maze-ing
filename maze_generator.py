dbbd33da187a40465b13d82637c6fbb49d7a3063
Adam Ghouaibia <adghouai@z4r9p5.42lyon.fr>
2026-04-29 18:39:32 +0200

    added output generator

--- maze_generator.py
+++ maze_generator.py
@@ -1,22 +1,8 @@
-from parsing_basemodel import fetch_config
+from parsing_basemodel import fetch_config, ValidConfig
+from typing import Callable
 import random
 import sys
-from enum import Enum
 sys.setrecursionlimit(99999999)
-
-
-class Directions(Enum):
-    NORTH = (0, -1)
-    SOUTH = (0, 1)
-    EAST = (1, 0)
-    WEST = (-1, 0)
-
-
-class WallValues(Enum):
-    TOP = 1
-    BOT = 4
-    EAST = 2
-    WEST = 8
 
 
 class MazeError(Exception):
@@ -39,7 +25,7 @@ class MazeGenerator:
             entry: tuple[int, int],
             exit: tuple[int, int],
             perfect: bool
-            ) -> None:
+    ) -> None:
         self.width = width
         self.height = height
         self.entry = entry
@@ -49,11 +35,11 @@ class MazeGenerator:
         if width >= 8 and height >= 6:
             self.add_42_pattern()
         else:
-            print("Could not add 42 pattern, maze is too small")
+            sys.stderr.write("Could not add 42 pattern, maze is too small")
         x, y = entry
         self.create_perfect_maze(self.layout[x][y])
         if perfect is False:
-            self.create_unperfect_maze()
+            self.create_imperfect_maze()
 
     @staticmethod
     def initialize_layout(x: int, y: int) -> list[list[Cell]]:
@@ -102,8 +88,6 @@ class MazeGenerator:
                      new_cell: Cell,
                      choice: tuple[int, int]
                      ) -> None:
-        if new_cell.visited is True:
-            return
         if choice == (0, -1):
             cell.walls -= 1
             new_cell.walls -= 4
@@ -117,11 +101,11 @@ class MazeGenerator:
             cell.walls -= 8
             new_cell.walls -= 2
 
-    def valid_direction(
+    def valid_neighbor_perfect(
             self,
             cell: Cell,
             random_direction: tuple[int, int]
-            ) -> bool:
+    ) -> bool:
         try:
             x1, y1 = cell.coordinates
             x2, y2 = random_direction
@@ -142,11 +126,11 @@ class MazeGenerator:
             (1, 0),
             (0, 1),
             (-1, 0)
-            ]
+        ]
         while directions:
             random_direction = random.choice(directions)
             directions.remove(random_direction)
-            if self.valid_direction(cell, random_direction):
+            if self.valid_neighbor_perfect(cell, random_direction):
                 availabe_paths.append(random_direction)
         return availabe_paths
 
@@ -157,10 +141,11 @@ class MazeGenerator:
             x1, y1 = cell.coordinates
             x2, y2 = path
             new_cell = self.layout[x1 + x2][y1 + y2]
-            self.destoy_walls(cell, new_cell, path)
+            if not new_cell.visited:
+                self.destoy_walls(cell, new_cell, path)
             self.create_perfect_maze(new_cell)
 
-    def create_unperfect_maze(self) -> None:
+    def create_imperfect_maze(self) -> None:
         x = 0
         while x < self.width:
             y = 0
@@ -186,15 +171,31 @@ class MazeGenerator:
         while coordinates_values and found_valid_cell is False:
             coordinates = random.choice(coordinates_values)
             coordinates_values.remove(coordinates)
-            try:
-                x, y = coordinates
-                cell: Cell = self.layout[x][y]
-                if cell.walls != 15:
-                    found_valid_cell = self.destroy_random_wall(cell)
-            except IndexError:
-                pass
+            x, y = coordinates
+            cell: Cell = self.layout[x][y]
+            if cell.walls != 15:
+                found_valid_cell = self.destroy_random_wall(cell)
 
-    def valid_neighbor(self, cell: Cell, wall: int) -> bool:
+    def destroy_random_wall(self, cell: Cell) -> bool:
+        wall_values = [8, 4, 2, 1]
+        x, y = cell.coordinates
+        if x == 0:
+            wall_values.remove(8)
+        if x == self.width - 1:
+            wall_values.remove(2)
+        if y == 0:
+            wall_values.remove(1)
+        if y == self.height - 1:
+            wall_values.remove(4)
+        while wall_values:
+            random_wall = random.choice(wall_values)
+            wall_values.remove(random_wall)
+            if random_wall & cell.walls:
+                if self.valid_neighbor_imperfect(cell, random_wall):
+                    return True
+        return False
+
+    def valid_neighbor_imperfect(self, cell: Cell, wall: int) -> bool:
         x, y = cell.coordinates
         try:
             if wall == 8:
@@ -213,30 +214,6 @@ class MazeGenerator:
             else:
                 neighbor.walls -= wall * 4
             return True
-        except IndexError:
-            return False
-
-    def destroy_random_wall(self, cell: Cell) -> bool:
-        try:
-            if cell.walls == 15:
-                return False
-            walls = [8, 4, 2, 1]
-            x, y = cell.coordinates
-            if x == 0:
-                walls.remove(8)
-            if x == self.width - 1:
-                walls.remove(2)
-            if y == 0:
-                walls.remove(1)
-            if y == self.height - 1:
-                walls.remove(4)
-            while walls:
-                wall = random.choice(walls)
-                walls.remove(wall)
-                if wall & cell.walls:
-                    if self.valid_neighbor(cell, wall):
-                        return True
-            return False
         except IndexError:
             return False
 
@@ -270,33 +247,77 @@ class BFS:
         for coordinates in valid_coordinates:
             self.path_list.append([coordinates])
 
-    def solve_maze(self) -> list[tuple[int, int]]:
+    def solve_maze(self) -> None:
         self.initialize_entry_paths()
         while self.path_list:
             for path in self.path_list:
-                if path[-1] == self.maze.exit:
-                    self.maze.path = path
                 x, y = path[-1]
                 cell: Cell = self.maze.layout[x][y]
                 cell.visited = True
                 valid_coordinates = self.find_directions(cell)
-                if len(valid_coordinates) >= 2:
-                    for coordinates in valid_coordinates:
-                        self.path_list.append(path + [coordinates])
-                    self.path_list.remove(path)
-                elif valid_coordinates:
-                    for coordinates in valid_coordinates:
-                        path.append(coordinates)
+                for coordinates in valid_coordinates:
+                    if coordinates == self.maze.exit:
+                        self.maze.path = path
+                        return
+                    self.path_list.append(path + [coordinates])
+                self.path_list.remove(path)
         raise MazeError("No path to exit found")
 
 
-def show_maze_output(maze: MazeGenerator) -> None:
-    """Test function"""
-    maze.output = []
-    for i in range(maze.height):
-        row = [maze.layout[j][i].walls for j in range(maze.width)]
-        maze.output.append(row)
-        print(row)
+def cardinal_path(maze: MazeGenerator) -> str:
+    path = ""
+    all_coordinates = ([maze.entry] + maze.path + [maze.exit])
+    for i in range(len(all_coordinates) - 1):
+        x1, y1 = all_coordinates[i]
+        x2, y2 = all_coordinates[i + 1]
+        if x1 > x2:
+            path += 'W'
+        elif x1 < x2:
+            path += 'E'
+        elif y1 > y2:
+            path += 'N'
+        elif y1 < y2:
+            path += 'S'
+    return (path + '\n')
+
+
+def generate_output_file(
+        maze: MazeGenerator,
+        output: str,
+        cardinal_path: str
+        ) -> None:
+    with open(output, "w") as file:
+        print(maze.height)
+        print(maze.width)
+        for y in range(maze.height):
+            row = [format(
+                maze.layout[x][y].walls, 'X') for x in range(maze.width)]
+            for walls in row:
+                file.write(walls)
+            file.write('\n')
+        file.write('\n' + str(maze.entry[0]) + ',' + str(maze.entry[1]))
+        file.write('\n' + str(maze.exit[0]) + ',' + str(maze.exit[1]))
+        file.write('\n' + cardinal_path)
+
+
+def maze_regenerator(config: ValidConfig) -> Callable:
+    def regen_maze() -> None:
+        try:
+            random.seed(None)
+            maze = MazeGenerator(
+                config.WIDTH,
+                config.HEIGHT,
+                config.ENTRY,
+                config.EXIT,
+                config.PERFECT
+            )
+            bfs = BFS(maze)
+            bfs.solve_maze()
+            generate_output_file(maze, config.OUTPUT_FILE, cardinal_path(maze))
+#           mlx
+        except Exception as e:
+            print(e)
+    return regen_maze
 
 
 def main() -> None:
@@ -314,9 +335,14 @@ def main() -> None:
             config.ENTRY,
             config.EXIT,
             config.PERFECT
-            )
+        )
         bfs = BFS(maze)
         bfs.solve_maze()
+        generate_output_file(maze, config.OUTPUT_FILE, cardinal_path(maze))
+
+#       mlx
+    except FileNotFoundError:
+        print(f"File {file} not found")
     except Exception as e:
         print(e)
 
