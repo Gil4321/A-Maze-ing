cd2c0c1d7e2c07312332e7572f42982626ed5c7e
Adam Ghouaibia <adghouai@z4r10p6.42lyon.fr>
2026-05-03 17:49:36 +0200

    modified main name

---
+++ a_maze_ing.py
@@ -0,0 +1,44 @@
+import sys
+from parsing_config import fetch_config
+from mazegen import MazeGenerator
+from output_generator import generate_output_file
+from maze_regenerator import maze_regenerator
+
+
+def main() -> None:
+    """
+    Program entry point.
+
+    Parses CLI arguments, loads configuration, generates a maze,
+    writes it to a file, and call the visualization part.
+
+    Raises:
+        ValueError: If the number of CLI arguments is invalid.
+    """
+    try:
+        if len(sys.argv) > 2:
+            raise ValueError("Too many arguments")
+        elif len(sys.argv) == 1:
+            raise ValueError("No arguments given")
+        file = sys.argv[1]
+        config = fetch_config(file)
+        maze = MazeGenerator(
+            config.WIDTH,
+            config.HEIGHT,
+            config.ENTRY,
+            config.EXIT,
+            config.PERFECT,
+            config.SEED,
+        )
+        generate_output_file(maze, config.OUTPUT_FILE)
+        regen = maze_regenerator(config)
+        from test_mlx import setup_and_run
+        setup_and_run(maze, regen)
+    except FileNotFoundError:
+        print(f"File {file} not found")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
